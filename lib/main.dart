673a53ca53c3180f3da74627c8e62455ad02d4f5
kimsehwan3013 <ksh-3013@naver.com>
2024-04-10 21:57:36 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:untitled3/screens/first_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstScreen(),
+    );
+  }
+}
